c226c782a988e34fca812eca68f187993d74ff21
Ali Moos <moosali446@gmail.com>
2025-10-16 05:46:30 +0000

    m

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - using printf specific character
+ * main - print a specific line using printf
  *
  * return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-
+	printf("with proper grammar, but the outcome is a piece of art,\ni");
 	return(0);
 }
