f0e035d462fda79279e1b0d1b4383348bb8a00ed
Ali Moos <moosali446@gmail.com>
2025-10-16 10:03:00 +0000

    m

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -2,11 +2,12 @@
 
 int main()
 {
-    printf("Size of a char: 1 byte(s)\n");
-    printf("Size of an int: 4 byte(s)\n");
-    printf("Size of a long int: 4 byte(s)\n");
-    printf("Size of a long long int: 8 byte(s)\n");
-    printf("Size of a float: 4 byte(s)\n");
-
-    return 0;
+	int 
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of an int: 4 byte(s)\n");
+	printf("Size of a long int: 4 byte(s)\n");
+	printf("Size of a long long int: 8 byte(s)\n");
+	printf("Size of a float: 4 byte(s)\n");
+	printf("sizeof(float)     = %d\n",sizeof(float));
+	return 0;
 }
