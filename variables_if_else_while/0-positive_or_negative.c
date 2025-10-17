91bdc9ba5c8eb1bd2e99dbb7c782943adfd796b0
Ali Moos <Moosali446@gmail.com>
2025-10-17 07:37:17 +0000

    m

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -10,6 +10,8 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* if the condition is true,then run this code */
+	/* if the number is greater than 0: is positive
+	 * if the number is 0: is zero
+	 * if the number is less than 0: is negative */
 	return (0);
 }
