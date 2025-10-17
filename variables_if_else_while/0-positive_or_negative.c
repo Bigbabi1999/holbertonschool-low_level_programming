b55a0cae892ba0395b412cac15f255217384001d
Ali Moos <Moosali446@gmail.com>
2025-10-17 07:07:11 +0000

    m

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+/* Function to display a welcome message */
+
+/* main - prints Hello, world
+* Return: 0 */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* if the condition is true,then run this code */
+	return (0);
+}
