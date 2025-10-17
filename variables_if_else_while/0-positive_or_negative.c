307642d91bb4e5071357a4eaf9c908a437da6463
Ali Moos <Moosali446@gmail.com>
2025-10-17 08:25:59 +0000

    m

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-/* Function to display a welcome message */
+/* Headers here */
 
 /* main - prints Hello, world
 * Return: 0 */
@@ -10,8 +10,6 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* if the number is greater than 0: is positive
-	 * if the number is 0: is zero
-	 * if the number is less than 0: is negative */
+	/* your code here */
 	return (0);
 }
