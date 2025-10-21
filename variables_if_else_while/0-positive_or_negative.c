5b6a6c52e4b7a5d563818d8dfcd43e6fe21b4553
Ali Moos <mooosali446@gmail.com>
2025-10-21 06:50:21 +0000

    m

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -4,15 +4,15 @@
 
 /**
 * main - prints Hello, world
-* Return: 0
+* Return:(0)
 */
 
 int main(void)
 {
 	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	srand (time(0));
+	n = rand () - RAND_MAX / 2;
 	printf ("enter a number");
 		scanf("%d",&n);
 	if (n > 0){ 
