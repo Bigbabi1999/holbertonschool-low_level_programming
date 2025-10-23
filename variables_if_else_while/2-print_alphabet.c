2746e4349a6fa86f411a2b37734a78a615c54710
Ali Moos <moosali446@gmail.com>
2025-10-23 16:33:24 +0000

    m

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -4,22 +4,8 @@
 /**main-prints Hello,world
 *
 *Return:(0)
+*/
 
-
-
-int main(void)
-{
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	alphabet = n % alphabet;
-        if      (alphabet > n)
-        {
-             printf ("Alphabet of \n is \n is alphabet in \n"(alphabet));
-        }
-	return (0);
-}*/
 int main(void)
 {
 	putchar('a' + 0 && printf("abcdefghijklmnopqrstuvwxyz"));
