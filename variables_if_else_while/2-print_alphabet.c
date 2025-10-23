4d74ba68bd34217f5f03e6a34018ce4b5ef4b5a2
Ali Moos <moosali446@gmail.com>
2025-10-23 09:58:53 +0000

    m

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/**main-prints Hello,world
+*
+*Return:(0)
+*/
+
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	alphabet = n % alphabet;
+        if      (alphabet > n)
+        {
+                ("Alphabet of \n is \n and is greater than 5\n",n,alphabet);
+        }
+	return (0);
+}
