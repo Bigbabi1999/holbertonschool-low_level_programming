b238d30617a7447cd8f548bb925b00d22caf71b5
Ali Moos <mooosali446@gmail.com>
2025-10-24 07:31:37 +0000

    m

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**main-prints Hello,world
+*
+*Return:(0)
+*/
+
+int main(void)
+{
+        int a = 0;
+
+        while(a < 26)
+        {
+                putchar('a'+ a);
+                a++;
+        }
+        putchar('\n');
+        return 0;
+}
